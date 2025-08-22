7d0668b7d16959384eddb32d791af780ae73b5fc
calliostro <calliostro@gmail.com>
2025-08-22 10:37:19 +0200

    Refactor PHPUnit configuration and enhance deprecation handling

--- src/DependencyInjection/CalliostroSpotifyWebApiExtension.php
+++ src/DependencyInjection/CalliostroSpotifyWebApiExtension.php
@@ -4,13 +4,13 @@ namespace Calliostro\SpotifyWebApiBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class CalliostroSpotifyWebApiExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
